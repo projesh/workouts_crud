1c863bc0e09a198a24d3a2305b985bc19ce90034
ps/jv <chkprojesh@gmail.com>
2023-08-07 17:11:33 +0545

    Adding cache feature and package

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@
 
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const apicache = require("apicache");
 // const v1Router = require("./v1/routers")
 const v1WorkoutRouter = require("./v1/routers/workoutRoutes")
 
 const app = express();
+const cache = apicache.middleware;
 const PORT = process.env.PORT || 3000;
 
 // app.use("/api/v1", v1Router);
 app.use(bodyParser.json());
-
+app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
